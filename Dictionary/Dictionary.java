5610f6629e51868a2ae3b0dbc597722dc9960aed
Keith Herbert <herbertkb@vcu.edu>
2014-06-10 23:54:39 -0400

    Fix 0005 Cleanup
    Deleted commented out code after testing Dictionary.jar on its own and
    with Translate.jar.

--- Dictionary/Dictionary.java
+++ Dictionary/Dictionary.java
@@ -36,102 +36,50 @@
 								simpler and faster Map data structure.
 
 
-
 ******************************************************************************/
 
 import java.util.Scanner;			// to parse the translation file
-//import java.util.Arrays;			// sort() and binarySearch()
-//import java.util.ArrayList;			// collection to hold translation file data
 import java.io.File;				// to work with scanner to open trans file
 import java.io.PrintStream; 		// to force printing in Unicode
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashMap;			// implementation of Map. Fastest lookup.
+import java.util.Map;				// Abstract data structure for mapping
 
 public class Dictionary {
-/*	Fix0005 - replacing Words object with Map<String, String>
-	static class Word implements Comparable<Word> {
-		String word;
-		String translation;
-
-		Word (String w, String t){
-			word = w;
-			translation = t;
-		}		
-
-		public int compareTo(Word w) {
-			return word.compareToIgnoreCase( w.getWord() );
-		}
-		
-		String getWord() { return word; }
-		String getTranslation() { return translation; }
-		
-	}
-*/
 	public static void main(String[] args) 
 		throws java.io.FileNotFoundException, 
 			   java.io.UnsupportedEncodingException {
 		
-		//	Parse commandline arguments.///////////////////////////////////////
+		/**	Parse commandline arguments.///////////////////////////////////////
 		//	args[0] must be the filename for the translation file//////////////
+		*/
 		String transFile = args[0];
 		
 		///////////////////////////////////////////////////////////////////////
-		// Open the translation file and read every line into memory as a 
+		/** Open the translation file and read every line into memory as a 
 		// key,value pair with the query word as the key and the translation
 		// as the value, represented in the translation file by two words
 		// separated by a tab.
+		*/
+		
 		Scanner parser = new Scanner(new File(transFile), "UTF-8");
 		Map<String, String> wordMap = new HashMap<String, String>();
-		
-		// Fix#0005 replace ArrayList with HashMap<String, String>
-		//ArrayList<Word> wordList = new ArrayList<Word>();
 		
 		while (parser.hasNextLine()) {
 			String line = parser.nextLine();
 			line = line.trim();
 			String[] fields = line.split("\t"); 
 			wordMap.put(fields[0], fields[1]);
-			
-			
-			// Fix#0005
-			//Word wordpair = new Word(fields[0], fields[1]);
-			//wordList.add(wordpair);
 		}
 		
-		/* Fix#005 - no longer needed because of Map
-		// Arrays.binarySearch() requires a pre-sorted array so we must
-		// convert the collection to an array and then sort it. 
-		wordList.trimToSize();
-		Word[] wordArray = wordList.toArray( new Word[wordList.size()]);
-		wordList = null;	// force garbage collection to save memory
-		Arrays.sort( wordArray );
-		*/
-		
-		
-		// Create input and output objects as Unicode to/from System In/Out.
+
+		///////////////////////////////////////////////////////////////////////
+		/** Create input and output objects as Unicode to/from System In/Out.
 		// This is needed to force Unicode over local encodings and handle
 		// non-Latin characters.
+		*/
+		
 		Scanner input = new Scanner(System.in, "UTF-8");
 		PrintStream output = new PrintStream(System.out, true, "UTF-8");
-		
-		/* Fix#0005 - binary searching replaced with Map.get(query)
-		// Main loop //////////////////////////////////////////////////////////
-		// Take query word from System.in, wrap as a Word object and perform 
-		// binarySearch to find it memory. If successful, print its translation.
-		// If not (binarySearch returns a negative int), print an error 
-		// message with the query word and the file name.
-		while ( true ) {
-			String query = input.next(); 
-			int queryIndex = Arrays.binarySearch(wordArray, new Word(query, ""));
-			if (queryIndex < 0) {
-				output.println( 
-					query.toUpperCase() + " not found in " + transFile + ".");
-			}
-			else {
-				output.println( wordArray[queryIndex].getTranslation());
-			}
-		}
-		*/
 		
 		// Main loop //////////////////////////////////////////////////////////
 		// Take query word from System.in and check if 
